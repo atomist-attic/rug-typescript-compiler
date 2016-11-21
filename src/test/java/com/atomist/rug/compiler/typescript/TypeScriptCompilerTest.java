4be65e6ec77b23eb0f371d465939785bc3e376a0
Christian Dupuis <cd@atomist.com>
2016-11-21 21:39:26 +0100

    Add some more tests (WIP)

--- src/test/java/com/atomist/rug/compiler/typescript/TypeScriptCompilerTest.java
+++ src/test/java/com/atomist/rug/compiler/typescript/TypeScriptCompilerTest.java
@@ -3,36 +3,35 @@ package com.atomist.rug.compiler.typescript;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Collection;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 import org.junit.Test;
 
 import com.atomist.rug.compiler.CompilerRegistry;
 import com.atomist.rug.compiler.ServiceLoaderCompilerRegistry;
+import com.atomist.rug.kind.core.ProjectMutableView;
 import com.atomist.source.ArtifactSource;
 import com.atomist.source.EmptyArtifactSource;
 import com.atomist.source.FileArtifact;
 import com.atomist.source.StringFileArtifact;
+import com.atomist.source.file.FileSystemArtifactSource;
+import com.atomist.source.file.SimpleFileSystemArtifactSourceIdentifier;
+
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
 import scala.collection.JavaConversions;
 
 public class TypeScriptCompilerTest {
 
-    private String editorTS = "//import Project from \"./Project\";\n" + 
-            "//import File from \"./Project\";\n" + 
-            "\n" + 
-            "/**\n" + 
-            "  Simple editor with no parameters\n" + 
-            "*/\n" + 
-            "class SimpleEditor implements ProjectEditor<Parameters> {\n" + 
-            "\n" + 
-            "    edit(project: Project, p: Parameters) {\n" + 
-            "        p.validate();\n" + 
-            "        project.addFile(\"src/from/typescript\", \"Anders Hjelsberg is God\");\n" + 
-            "        return `Edited Project now containing ${project.fileCount()} files: \\n`;\n" + 
-            "    }\n" + 
-            "}";
+    private String editorTS = "class SimpleEditor  {\n" + "\n" + "    edit() {\n"
+            + "        return \"yeah\"\n" + "    }\n" + "}\n" + "";
 
     @Test
     public void testCompile() {
@@ -45,6 +44,41 @@ public class TypeScriptCompilerTest {
         assertTrue(compiler.supports(source));
         ArtifactSource result = compiler.compile(source);
         assertTrue(result.findFile(".atomist/editors/MyEditor.js").isDefined());
+    }
+
+    @Test
+    public void testCompileUserModel() throws ScriptException {
+        ArtifactSource source = new FileSystemArtifactSource(
+                new SimpleFileSystemArtifactSourceIdentifier(new File("../user-model")));
+        
+        TypeScriptCompiler compiler = new TypeScriptCompiler();
+        assertTrue(compiler.supports(source));
+        ArtifactSource result = compiler.compile(source);
+        
+        String jsContents = result.findFile(".atomist/editors/SimpleEditor.js").get().content();
+        
+        ScriptEngine engine = new ScriptEngineManager(null).getEngineByName("nashorn");
+        engine.eval("exports = {}");
+        engine.eval(jsContents);
+        engine.eval("var editor = new SimpleEditor();");
+        ScriptObjectMirror editor = (ScriptObjectMirror) engine.getContext().getBindings(ScriptContext.ENGINE_SCOPE).get("editor");
+        
+        ProjectMutableView pmv = new ProjectMutableView(new EmptyArtifactSource(""), source);
+        String resultString = (String) editor.callMember("edit", pmv, new Object());
+        
+        System.setProperty("RUG_TYPINGS", "/Users/cdupuis/Development/atomist/user-model/.atomist/editors/");
+        
+        jsContents = result.findFile(".atomist/editors/ConstructedEditor.js").get().content();
+        engine = new ScriptEngineManager(null).getEngineByName("nashorn");
+        engine.eval("exports = {}");
+        engine.eval(source.findFile(".atomist/typings/jvm-npm.js").get().content());
+        engine.eval(jsContents);
+        engine.eval("var editor = new ConstructedEditor();");
+        editor = (ScriptObjectMirror) engine.getContext().getBindings(ScriptContext.ENGINE_SCOPE).get("editor");
+        
+//        ProjectMutableView pmv = new ProjectMutableView(new EmptyArtifactSource(""), source);
+//        String resultString = (String) editor.callMember("edit", pmv, new Object());
+//        System.out.println(resultString + " " + JavaConversions.asJavaCollection(source.allFiles()).size());
     }
 
     @Test
